45241d0b4e6374a849b18ff3dff81d7c4721567a
Foridul35962 <foridulislam35962@gmail.com>
2026-01-01 19:05:12 +0600

    router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import MainLayout from './layout/MainLayout'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Registration from './pages/Registration'
+import ForgetPass from './pages/ForgetPass'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+    ]
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/registration',
+    element: <Registration />
+  },
+  {
+    path: '/forget-pass',
+    element: <ForgetPass />
+  }
+])
 
 const App = () => {
   return (
-    <div className='text-red-600'>App</div>
+    <>
+      {
+        <RouterProvider router={router} />
+      }
+    </>
   )
 }
 
